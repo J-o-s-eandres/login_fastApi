8c9437e7d56c0b9998bf2ae715c3f2287ebed0fd
Joseandres Montesino <joseandresmontesino@gmail.com>
2023-12-27 18:17:51 -0500

    add comments

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ class AuthHandler:
     @staticmethod
     def get_user(db, username: str):
         if username in db:
+            # Convertir el objeto User a un diccionario para poder usar ** en la instancia User
             user_dict = db[username].__dict__
             return User(**user_dict)
         return None
@@ -43,7 +44,10 @@ def create_jwt_token(data: dict):
 
 @app.post("/token")
 async def login_for_access_token(username: str = Form(...), password: str = Form(...)):
+    # Obtener el usuario de la base de datos ficticia
     user = AuthHandler.get_user(fake_users_db, username)
+    
+    # Verificar las credenciales del usuario
     if not user or not AuthHandler.verify_password(password, user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -51,13 +55,17 @@ async def login_for_access_token(username: str = Form(...), password: str = Form
             headers={"WWW-Authenticate": "Bearer"},
         )
 
+    # Crear un token JWT válido
     token_data = {"sub": user.username}
     token = create_jwt_token(token_data)
+    
+    # Devolver el token de acceso
     return {"access_token": token, "token_type": "bearer"}
 
 @app.get("/protected")
 async def protected_route(token: str = Depends(oauth2_scheme)):
     try:
+        # Decodificar el token JWT y obtener el nombre de usuario
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
@@ -65,4 +73,5 @@ async def protected_route(token: str = Depends(oauth2_scheme)):
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
+    # Devolver un mensaje con el nombre de usuario para la ruta protegida
     return {"message": "You have access to this protected route", "username": username}
